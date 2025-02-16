7ac412a5f15a046a0c2c8c50dce0e42c9975004d
Patrick Bartosik <patrickbartosik@Host-002.lan>
2025-02-16 17:51:55 +0100

    🔧 (capacitor.config.ts): update appName and server URL for local development ✨ (next.config.ts): add unoptimized image configuration ♻️ (page.tsx): simplify SearchPage component by removing unused code ♻️ (book-list-detail.tsx): simplify BookListDetail component by removing unused code ♻️ (calendar.tsx): remove unused icon components from Calendar configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'bookish.test.app',
-  appName: 'bookish',
+  appName: 'bookish-test',
   webDir: 'src/app',
   server: {
     androidScheme: 'http',
-    url: process.env.NEXT_PUBLIC_CAPACITOR_URL,
+    url: 'http://192.168.1.88:3000',
     cleartext: true
   },
   plugins: {
