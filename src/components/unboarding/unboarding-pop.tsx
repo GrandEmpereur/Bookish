0f9db728fc5ab574009e30ad81e3ded378c297a5
Ricardo <tchamgouericardo@gmail.com>
2025-07-10 15:59:24 +0200

    message and unboarding

--- src/components/unboarding/unboarding-pop.tsx
+++ src/components/unboarding/unboarding-pop.tsx
@@ -3,8 +3,7 @@
 import { useState } from "react";
 
 export default function Unboarding() {
-  const [showPopup, setShowPopup] = useState(true); // Affiché par défaut
-
+  const [showPopup, setShowPopup] = useState(true);
   const handleClose = () => {
     setShowPopup(false);
   };
@@ -13,23 +12,25 @@ export default function Unboarding() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-2xl shadow-lg p-6 text-center w-[335px] h-[295px] relative">
-        
+      <div className="bg-white rounded-2xl shadow-lg p-6 text-center w-[345px] h-[295px] relative">
         <div className="flex justify-center items-center mb-4 relative">
           <img
-            src="/img/etoile-left.png"
+            src="/img/unboarding/etoile-left.png"
             alt="Star Left"
-            className="w-4 h-4 absolute left-8 top-2"
+            className="w-24 h-16 absolute left-8 top-2"
           />
-          <img src="/img/ours.png" alt="Bear" className="w-16 h-16 z-10" />
           <img
-            src="/img/etoile-right.png"
+            src="/img/unboarding/ours.png"
+            alt="Bear"
+            className="w-16 h-16 z-10"
+          />
+          <img
+            src="/img/unboarding/etoile-right.png"
             alt="Star Right"
-            className="w-4 h-4 absolute right-8 top-2"
+            className="w-24 h-16 absolute right-8 top-2"
           />
         </div>
 
-      
         <h2 className="font-bold text-lg mb-2">
           Bienvenue dans l’univers de Bookish !
         </h2>
@@ -38,10 +39,9 @@ export default function Unboarding() {
           suivez vos avancées de lecture
         </p>
 
-        
         <button
           onClick={handleClose}
-          className="bg-orange-500 text-white px-4 py-2 rounded-full text-sm font-medium"
+          className="bg-orange-500 cursor-pointer text-white px-4 py-2 rounded-full text-sm font-medium"
         >
           Je commence !
         </button>
